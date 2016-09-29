f9ae5bcad2f050bbbfbea8012c25c2ea30004c44
Emmett Tan <UBCEmmettTan@gmail.com>
2016-09-28 22:13:08 -0700

    Improved log efficiency

--- score.rb
+++ score.rb
@@ -2,11 +2,15 @@ class Score
     #Initialize frequency map of common english four letter sequences
     def initialize
         @freqhash = Hash.new
+        @log_lookup = Hash.new
         @score_sum = 0
         File.open("english_quadgrams.txt", "r").each_line do |line|
             data = line.split(" ")
             @freqhash[data[0]] = Integer(data[1])
-            @score_sum += Integer(data[1])
+            @score_sum += Integer(data[1])    
+        end
+        @freqhash.each do |key, value|
+            @log_lookup[key] = Math::log10(value.to_f/@score_sum)
         end
         @shitty_quad = Math::log10(0.02/@score_sum.to_f)
     end
@@ -34,32 +38,32 @@ class Score
         #puts "Scoring input: #{input}"
         #puts "==========================="
         quadgraphArr.each { |str|
-            if (@freqhash.has_key?(str.upcase))
-                str_score = Math::log10(@freqhash[str.upcase]/@score_sum.to_f)
+            if (@log_lookup.has_key?(str.upcase))
+                str_score = @log_lookup[str.upcase]
             else
                 str_score = @shitty_quad
             end
             final_score += str_score
         }
         quadgraphArr2.each { |str|
-            if (@freqhash.has_key?(str.upcase))
-                str_score = Math::log10(@freqhash[str.upcase]/@score_sum.to_f)
+            if (@log_lookup.has_key?(str.upcase))
+                str_score = @log_lookup[str.upcase]
             else
                 str_score = @shitty_quad
             end
             final_score += str_score
         }
         quadgraphArr3.each { |str|
-            if (@freqhash.has_key?(str.upcase))
-                str_score = Math::log10(@freqhash[str.upcase]/@score_sum.to_f)
+            if (@log_lookup.has_key?(str.upcase))
+                str_score = @log_lookup[str.upcase]
             else
                 str_score = @shitty_quad
             end
             final_score += str_score
         }
         quadgraphArr4.each { |str|
-            if (@freqhash.has_key?(str.upcase))
-                str_score = Math::log10(@freqhash[str.upcase]/@score_sum.to_f)
+            if (@log_lookup.has_key?(str.upcase))
+                str_score = @log_lookup[str.upcase]
             else
                 str_score = @shitty_quad
             end
